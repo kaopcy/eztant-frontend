2b56d6689f68180ffed2e2db84c241242fbfa623
kaopcy <kaonaka@hotmail.co.th>
2022-03-17 19:02:04 +0700

    fix navbar

--- src/component/navbar/MobileDropdown.js
+++ src/component/navbar/MobileDropdown.js
@@ -1,10 +1,12 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { Fragment, useEffect, useRef, useState } from "react";
 import {
     faMagnifyingGlass,
     faChevronLeft,
+    faChevronDown,
 } from "@fortawesome/free-solid-svg-icons";
 
-import { Link, useLocation, useMatch, useResolvedPath } from "react-router-dom";
+import { Link, useMatch, useResolvedPath } from "react-router-dom";
+import { Disclosure } from "@headlessui/react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import gsap from "gsap";
 
@@ -58,19 +60,30 @@ const MobileDropdown = (props) => {
                 />
             </div>
             <Searchbar handleInput={handleInput} />
-            <div className="flex w-full flex-col space-y-2 mt-10">
-                {links.map((link) => (
-                    <CustomLink
-                        className="shrink-0 text-xl font-medium text-gray-600 px-10 w-full"
-                        to={link.to}
-                        state={
-                            link.modal ? { backgroundLocation: location } : null
-                        }
-                        key={link.name}
-                    >
-                        {link.name}
-                    </CustomLink>
-                ))}
+            <div className="mt-10 flex w-full flex-col items-center">
+                {links.map((link) =>
+                    link.children ? (
+                        <DisclosureLink
+                            key={link.name}
+                            link={link}
+                            handleOnClose={handleOnClose}
+                        ></DisclosureLink>
+                    ) : (
+                        <CustomLink
+                            className="w-[90%] shrink-0 rounded-md px-10 py-3 text-xl font-medium text-gray-600"
+                            to={link.to}
+                            state={
+                                link.modal
+                                    ? { backgroundLocation: location }
+                                    : null
+                            }
+                            key={link.name}
+                            handleOnClose={handleOnClose}
+                        >
+                            {link.name}
+                        </CustomLink>
+                    )
+                )}
             </div>
         </div>
     );
@@ -93,18 +106,80 @@ const Searchbar = (props) => {
     );
 };
 
-const CustomLink = ({ children, to, ...props }) => {
+const CustomLink = ({
+    children,
+    to,
+    handleOnClose,
+    defaultColor,
+    ...props
+}) => {
     const { pathname: path } = useResolvedPath(to);
     const match = useMatch({ path, end: true });
-
+    const style = {
+        color: match ? "white" : "rgb(75,85,99)",
+        background: match ? "#465FFC" : defaultColor ?? "white",
+    };
     return (
         <Link
-            style={{ color: match ? "red" : "rgb(75,85,99)" }}
+            style={style}
             to={to}
             {...props}
+            onClick={match ? () => handleOnClose() : null}
         >
             {children}
         </Link>
+    );
+};
+
+const DisclosureLink = (props) => {
+    const { link: data } = props;
+    const menu = data.children;
+
+    const Link = (props) => {
+        const { handleOnClose, link } = props;
+        return (
+            <CustomLink
+                className="w-[90%] shrink-0 rounded-md bg-slate-50 px-10 py-3 text-xl font-medium text-gray-600"
+                to={link.to}
+                key={link.name}
+                state={null}
+                handleOnClose={handleOnClose}
+                defaultColor={"rgb(248,250,252)"}
+            >
+                {link.name}
+            </CustomLink>
+        );
+    };
+
+    return (
+        <Disclosure>
+            {({ open }) => (
+                <>
+                    <Disclosure.Button as={Fragment}>
+                        <div
+                            className={`flex w-[90%] shrink-0 justify-between rounded-md px-10 py-3 text-xl font-medium text-gray-600 ${
+                                open ? "bg-slate-50" : "bg-white"
+                            }`}
+                        >
+                            <span>{data.name}</span>
+                            <FontAwesomeIcon
+                                icon={faChevronDown}
+                                className={`${
+                                    open ? "rotate-180" : ""
+                                } transition-transform duration-500 `}
+                            />
+                        </div>
+                    </Disclosure.Button>
+                    <Disclosure.Panel
+                        className={`flex w-[90%] flex-col bg-slate-50 pl-14`}
+                    >
+                        {menu.map((e) => (
+                            <Link key={e.name} {...props} link={e}></Link>
+                        ))}
+                    </Disclosure.Panel>
+                </>
+            )}
+        </Disclosure>
     );
 };
 
--- src/component/navbar/Navbar.js
+++ src/component/navbar/Navbar.js
@@ -37,6 +37,56 @@ const Navbar = () => {
             name: "โพสต์",
             to: "register",
             modal: true,
+            children: [
+                {
+                    name: "รวมทุกภาควิชา",
+                    to: "/elec",
+                },
+                {
+                    name: "การเกษตร",
+                    to: "/elec",
+                },
+                {
+                    name: "คอมพิวเตอร์",
+                    to: "/elec",
+                },
+                {
+                    name: "เคมี",
+                    to: "/elec",
+                },
+                {
+                    name: "เครื่องกล",
+                    to: "/elec",
+                },
+                {
+                    name: "ชีวการแพทย์",
+                    to: "/elec",
+                },
+                {
+                    name: "ดนตรี",
+                    to: "/elec",
+                },
+                {
+                    name: "โทรคมนาคม",
+                    to: "/elec",
+                },
+                {
+                    name: "ไฟฟ้า",
+                    to: "/elec",
+                },
+                {
+                    name: "โยธา",
+                    to: "/elec",
+                },
+                {
+                    name: "อาหาร",
+                    to: "/elec",
+                },
+                {
+                    name: "อิเล็กโทรนิคส์",
+                    to: "/elec",
+                },
+            ],
         },
         {
             name: "คอมมูนิตี้",
