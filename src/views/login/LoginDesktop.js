e7f39fec7e92b32244d899e1ec6823f8b4caadc1
kaopcy <kaonaka@hotmail.co.th>
2022-03-21 19:30:51 +0700

    :art: improve login animation load time

--- src/views/login/LoginDesktop.js
+++ src/views/login/LoginDesktop.js
@@ -1,21 +1,31 @@
-import React, { Fragment, forwardRef, useRef, useEffect } from "react";
+import React, {
+    Fragment,
+    forwardRef,
+    useRef,
+    useEffect,
+    useContext,
+} from "react";
 import { useState } from "react";
 import { Dialog, Transition } from "@headlessui/react";
 import { useNavigate } from "react-router-dom";
 import gsap, { Back } from "gsap";
+import axios from "axios";
+
+import { faChevronLeft , faCheckCircle } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import { ReactComponent as EztantLogo } from "../../assets/logos/eztant.svg";
 import { ReactComponent as DecorationImage } from "../../assets/images/login-decoration.svg";
 import GoogleRegister from "../../component/register/GoogleRegister";
 
-import { faChevronLeft } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
+// Context
+import { AuthContext } from "../../composables/context/auth";
 import {
-    UserinputProvider,
     useUserinput,
+    UserinputProvider,
     useHandleUserinputUpdate,
 } from "../../composables/context/useUserinputContext";
+import { useNonInitialEffect } from "../../composables/useNonInitialEffect";
 
 const LoginDesktop = (props) => {
     const [isOpen, setIsOpen] = useState(true);
@@ -60,7 +70,7 @@ const LoginDesktop = (props) => {
                                 <MainBody
                                     ref={ref}
                                     {...props}
-                                    onClose={() => setIsOpen(false)}
+                                    onClose={() => handleCloseModal()}
                                 />
                             )}
                         </Transition.Child>
@@ -75,7 +85,40 @@ const MainBody = forwardRef((props, ref) => {
     const decorationContainer = useRef(null);
     const inputContainer = useRef(null);
     const mainContainer = useRef(null);
-    // const tl = useRef(null);
+    
+    const finishedAnimation = () => {
+        const tl = gsap.timeline();
+        tl.to(inputContainer.current, {
+            xPercent: 100,
+            duration: 1.3,
+            ease: "power4.inOut",
+        })
+
+            .fromTo(
+                mainContainer.current,
+                {
+                    width: `${mainContainer.current.offsetWidth}px`,
+                },
+                {
+                    width: `${mainContainer.current.offsetWidth * 0.5}px`,
+                    ease: "power2.inOut",
+                }
+            )
+            .to(
+                decorationContainer.current,
+                {
+                    width: mainContainer.current.offsetWidth * 0.5,
+                    position: "absolute",
+                    left: "0",
+                    ease: "power4.inOut",
+                },
+                "<"
+            )
+            .to(decorationContainer.current, {
+                yPercent: 100,
+                ease: "power4.in",
+            }, '-=0.75');
+    };
 
     useEffect(() => {
         const mainConWidth = mainContainer.current.offsetWidth;
@@ -98,15 +141,6 @@ const MainBody = forwardRef((props, ref) => {
             "<0.5"
         )
             .from(
-                "#dec-img",
-                {
-                    yPercent: 100,
-                    ease: Back.easeOut.config(1.8),
-                    duration: 0.7,
-                },
-                "<0.6"
-            )
-            .from(
                 ".animated-text",
                 {
                     xPercent: (i) => Math.pow(-1, i + 1) * 100,
@@ -114,8 +148,11 @@ const MainBody = forwardRef((props, ref) => {
                         amount: 0.2,
                     },
                 },
-                "<"
-            );
+                "<0.6"
+            )
+            .set(inputContainer.current, {
+                width: "60%",
+            });
     }, []);
 
     return (
@@ -124,19 +161,84 @@ const MainBody = forwardRef((props, ref) => {
             ref={ref}
         >
             <div
-                className="flex-cen relative h-full w-full justify-end overflow-hidden bg-primary text-text shadow-md"
+                className=" flex-cen relative h-full w-full shrink-0 justify-end overflow-hidden bg-primary text-text shadow-md"
                 ref={mainContainer}
             >
                 <button></button>
                 <DecorationContainer ref={decorationContainer} />
-                <InputContainer ref={inputContainer} />
+                <InputContainer
+                    ref={inputContainer}
+                    {...props}
+                    finishedAnimation={finishedAnimation}
+                />
             </div>
         </div>
     );
 });
 
-const InputContainer = forwardRef((_, ref) => {
-    const navigate = useNavigate()
+const InputContainer = forwardRef((props, ref) => {
+    const { onClose, finishedAnimation } = props;
+    const navigate = useNavigate();
+
+    const userinput = useUserinput();
+    const { login } = useContext(AuthContext);
+    const [isLoading, setIsLoading] = useState(false);
+    const loadingRef = useRef(null);
+    const loadingTextRef = useRef(null);
+
+    const handleLogin = async (e) => {
+        setIsLoading(true);
+        const data = await new Promise((resolve) => {
+            setTimeout(async () => {
+                const res = await axios.get(
+                    `https://randomuser.me/api/?seed=${userinput.email}`
+                );
+                const fakeUser = res.data.results[0];
+                resolve(fakeUser);
+            }, 100);
+        });
+        setIsLoading(false);
+        console.log(data);
+        finishedAnimation();
+        login({
+            firstname: data.name.first,
+            lastname: data.name.last,
+            gender: data.gender,
+        });
+    };
+
+    useNonInitialEffect(() => {
+        const tl2 = gsap.timeline({ repeat: -1 });
+        const tl = gsap.timeline();
+        if (isLoading) {
+            tl2.to(loadingRef.current, {
+                rotate: "+=360",
+                ease: "linear",
+            });
+            tl.to(loadingTextRef.current, {
+                x: -30,
+            }).to(
+                loadingRef.current,
+                {
+                    x: 30,
+                    opacity: 1,
+                },
+                "<"
+            );
+        } else {
+            tl.to(loadingTextRef.current, {
+                x: 0,
+            }).to(
+                loadingRef.current,
+                {
+                    x: 0,
+                    opacity: 0,
+                },
+                "<"
+            );
+        }
+    }, [isLoading]);
+
     return (
         <div
             className="flex-col-cen h-full w-[60%] shrink-0 space-y-6 rounded-l-3xl bg-white shadow-md"
@@ -159,7 +261,7 @@ const InputContainer = forwardRef((_, ref) => {
                     <div className="input-group mt-4 flex w-full items-center justify-center space-x-8">
                         <button
                             className=" group flex h-12 w-[11rem] items-center justify-center space-x-2 rounded-2xl border-4 border-secondary px-2 py-1 hover:bg-secondary"
-                            onClick={() => navigate(-1)}
+                            onClick={() => onClose()}
                         >
                             <FontAwesomeIcon
                                 className="text-lg text-secondary group-hover:text-white"
@@ -169,18 +271,41 @@ const InputContainer = forwardRef((_, ref) => {
                                 กลับ
                             </span>
                         </button>
-                        <button className=" flex h-12 w-full items-center justify-center space-x-2 rounded-2xl border-4 border-secondary bg-secondary px-6 py-1">
-                            <span className="text-lg text-white">
-                                ลงทะเบียน
+                        <button
+                            className="btn-orange flex-cen group h-12 w-full  space-x-3 rounded-2xl border-4 px-6 py-1"
+                            onClick={() => handleLogin()}
+                        >
+                            <span className="flex-cen relative text-lg">
+                                <div ref={loadingTextRef}>เข้าสุ่ระบบ</div>
+                                <div
+                                    ref={loadingRef}
+                                    className="loading sp absolute h-4 w-4 self-center opacity-0 group-hover:border-secondary"
+                                ></div>
                             </span>
                         </button>
                     </div>
                 </div>
             </div>
             <div className="flex-cen mt-8 w-[80%] items-center justify-center space-x-1">
-                <span className="text-xs text-gray-400">มีบัญชีอยู่แล้ว?</span>
-                <span className="cursor-pointer text-xs text-blue-800 underline ">
-                    เข้าสู่ระบบ
+                <span className="text-xs text-gray-400">ยังไม่มีบัญชี?</span>
+                <span
+                    className="cursor-pointer text-xs text-blue-800 underline "
+                    onClick={async () => {
+                        await navigate("/");
+                        await navigate("/register", {
+                            state: {
+                                backgroundLocation: {
+                                    pathname: "/",
+                                    search: "",
+                                    hash: "",
+                                    state: null,
+                                    key: "vgcp8l3i",
+                                },
+                            },
+                        });
+                    }}
+                >
+                    สมัครบัญชีใหม่
                 </span>
             </div>
         </div>
@@ -206,10 +331,13 @@ const Input = (props) => {
 };
 
 const DecorationContainer = forwardRef((_, ref) => {
+    const finishedOverlay = useRef(null)
+    const navigate = useNavigate()
+    
     const Text = ({ children }) => {
         return (
             <div className="overflow-hidden">
-                <div className="animated-text whitespace-nowrap text-[35px] font-bold tracking-[2px] text-white ">
+                <div className="animated-text whitespace-nowrap text-[30px] font-bold tracking-[2px] text-white lg:text-[35px] ">
                     {children}
                 </div>
             </div>
@@ -233,10 +361,44 @@ const DecorationContainer = forwardRef((_, ref) => {
             </div>
             <DecorationImage
                 id="dec-img"
-                className="absolute bottom-0 left-0 w-[110%]"
+                className="absolute bottom-0 left-0 w-[110%] max-w-[440px] z-10"
             />
+            <div
+                ref={finishedOverlay}
+                className="absolute top-0 left-0 flex h-full w-full flex-col items-center justify-center space-y-10 bg-transparent"
+            >
+                <span className=" stagger-animation text-3xl">ลงทะเบียน</span>
+                <span className=" stagger-animation h-[2px] w-3/4 bg-white"></span>
+                <span className=" stagger-animation text-[4vw]">
+                    เสร็จเรียบร้อย
+                </span>
+                <FontAwesomeIcon
+                    className="stagger-animation text-[80px]"
+                    icon={faCheckCircle}
+                />
+                <div
+                    className="stagger-animation flex-cen w-2/3 cursor-pointer rounded-full border-4 border-white bg-white py-4 text-xl text-primary hover:border-white hover:bg-primary hover:text-white"
+                    onClick={async () => {
+                        await navigate("/");
+                        await navigate("/login", {
+                            state: {
+                                backgroundLocation: {
+                                    pathname: "/",
+                                    search: "",
+                                    hash: "",
+                                    state: null,
+                                    key: "vgcp8l3i",
+                                },
+                            },
+                        });
+                    }}
+                >
+                    เข้าสุ่ระบบ
+                </div>
+            </div>
         </div>
     );
 });
+
 
 export default LoginDesktop;
