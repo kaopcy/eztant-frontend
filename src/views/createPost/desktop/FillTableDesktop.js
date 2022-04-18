e05b2303afce1ab729433e0f90728f1e875866d6
kaopcy <kaonaka@hotmail.co.th>
2022-04-18 13:24:01 +0700

    :heavy_plus_sign: add react-hook-form

--- src/views/createPost/desktop/FillTableDesktop.js
+++ src/views/createPost/desktop/FillTableDesktop.js
@@ -1,57 +1,94 @@
-import React, { memo, useEffect, useLayoutEffect, useRef, useState } from "react";
+import React, { forwardRef, useEffect, useLayoutEffect, useRef, useState } from "react";
 import gsap from "gsap";
+import { Link, useMatch } from "react-router-dom";
+import { useFieldArray, useForm } from "react-hook-form";
 
 import { useHandleTableInput, useTableInput, useAddTableInput, useDeleteTableInput } from "../context/tableCreatePostContext";
 
-import { faPlus, faClose } from "@fortawesome/free-solid-svg-icons";
+import { faPlus, faClose, faChevronRight } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// thisi si another test for fill table
+const TABLE_ROW_HEIGHT = 44;
 
 const FillTableDesktop = () => {
+    const tableMatch = useMatch("/create-post/fill-table");
+    const detailMatch = useMatch("/create-post");
+
+    const { register, handleSubmit, control, watch } = useForm();
+
+
     return (
-        <div className="mt-10 flex w-full items-start justify-center space-x-4 px-2 lg:space-x-20 ">
-            <Table className="" />
-            <DueDate className="" />
-        </div>
+        <>
+            <form
+                onSubmit={handleSubmit(data => {
+                    console.log(data);
+                })}
+                className="mt-10 flex w-full items-start justify-center space-x-4 px-2 lg:space-x-20 ">
+                <Table ref={register} control={control} />
+                <DueDate />
+            </form>
+            <div className="mt-20 flex w-full items-center justify-center space-x-2">
+                <Link to="/create-post" className="">
+                    <FontAwesomeIcon
+                        icon={faChevronRight}
+                        className={`rotate-180  border-2 py-3 px-2 transition-all  ${
+                            tableMatch
+                                ? "cursor-pointer border-gray-400 hover:bg-text hover:text-white"
+                                : "cursor-default border-slate-100 text-slate-200"
+                        }`}
+                    />
+                </Link>
+                <Link className="" to="fill-table">
+                    <FontAwesomeIcon
+                        icon={faChevronRight}
+                        className={` border-2  py-3 px-2 transition-all  ${
+                            detailMatch
+                                ? "cursor-pointer border-gray-400 hover:bg-text hover:text-white"
+                                : "cursor-default border-slate-100 text-slate-200"
+                        }`}
+                    />
+                </Link>
+            </div>
+        </>
     );
 };
 
-const TABLE_ROW_HEIGHT = 44;
+const Table = forwardRef(({ control }, register) => {
+    // const tableInput = useTableInput();
+    // const addTable = useAddTableInput();
+    const { fields, append, remove } = useFieldArray({ control, name: "tables" });
 
-const Table = ({ className }) => {
-    const tableInput = useTableInput();
-    const addTable = useAddTableInput();
+    // const tableContainer = useRef(null);
+    // const tl = useRef(null);
+    // const tl2 = useRef(null);
 
-    const tableContainer = useRef(null);
-    const tl = useRef(null);
-    const tl2 = useRef(null);
+    // useLayoutEffect(() => {
+    //     gsap.set(tableContainer.current, { height: `+=${TABLE_ROW_HEIGHT * (tableContainer.current.childElementCount - 1)}px` });
+    // }, []);
 
-    useLayoutEffect(() => {
-        gsap.set(tableContainer.current, { height: `+=${TABLE_ROW_HEIGHT * (tableContainer.current.childElementCount - 1)}px` });
-    }, []);
+    // useEffect(() => {
+    //     tl.current = gsap.to(tableContainer.current, {
+    //         paused: true,
+    //         height: `+=${TABLE_ROW_HEIGHT}`,
+    //     });
+    //     tl2.current = gsap.to(tableContainer.current, {
+    //         paused: true,
+    //         height: `-=${TABLE_ROW_HEIGHT}`,
+    //     });
+    // }, [tableInput.length]);
 
-    useEffect(() => {
-        tl.current = gsap.to(tableContainer.current, {
-            paused: true,
-            height: `+=${TABLE_ROW_HEIGHT}`,
-        });
-        tl2.current = gsap.to(tableContainer.current, {
-            paused: true,
-            height: `-=${TABLE_ROW_HEIGHT}`,
-        });
-    }, [tableInput.length]);
+    // const play = () => {
+    //     tl2.current.play();
+    // };
 
-    const play = () => {
-        tl2.current.play();
-    };
-
-    useEffect(() => {
-        return () => {
-            tl.current.kill();
-        };
-    }, []);
+    // useEffect(() => {
+    //     return () => {
+    //         tl.current.kill();
+    //     };
+    // }, []);
 
     return (
-        <div className={`flex flex-col items-center ${className} mr-10`}>
+        <div className={`mr-10 flex min-h-[323px] flex-col  items-center`}>
             <div className="flex  flex-col items-start ">
                 <div className="z-10 flex w-full bg-gray-300 text-text">
                     <div className=" shrink-0 border-r ">
@@ -71,13 +108,21 @@ const Table = ({ className }) => {
                     </div>
                     <div className="flex w-full items-center justify-center px-6 font-bold">เวลา</div>
                 </div>
-                <div ref={tableContainer} className="relative z-0 flex flex-col items-start ">
+                {fields.map(({ id, ...item }, index) => (
+                    <div className="" key={id}>
+                        <NewAddTable index={index} ref={register} {...item} />
+                    </div>
+                ))}
+                <div className="btn-orange px-4 py-2 " onClick={() => append({})}>
+                    Add
+                </div>
+                {/* <div ref={tableContainer} className="relative z-0 flex flex-col items-start ">
                     <AddedTable index={0} tableInput={tableInput[0]} main onDelete={play} />
                     {tableInput.map((e, index) => index !== 0 && <AddedTable onDelete={play} key={index} index={index} tableInput={e} />)}
-                </div>
+                </div> */}
             </div>
-            
-            <div
+
+            {/* <div
                 onClick={() => {
                     addTable();
                     tl.current.play();
@@ -85,12 +130,12 @@ const Table = ({ className }) => {
                 className="btn-white flex-cen mt-10 space-x-2 rounded-xl border-2 px-5 py-1 text-lg">
                 <FontAwesomeIcon className="text-sm" icon={faPlus} />
                 <div className="">เพิ่ม</div>
-            </div>
+            </div> */}
         </div>
     );
-};
+});
 
-const AddedTable = memo(({ index, tableInput, onDelete, main = false }) => {
+const AddedTable = ({ index, tableInput, onDelete, main = false }) => {
     const rowTable = useRef(null);
     const tl = useRef(null);
 
@@ -186,29 +231,23 @@ const AddedTable = memo(({ index, tableInput, onDelete, main = false }) => {
             </div>
         </div>
     );
-});
-
-const Text = () => {
-    const table = useTableInput();
-    return (
-        <div className="">
-            {table.map((e, index) => (
-                <div className="flex" key={index}>
-                    <div className="w-20">{e.section}</div>
-                    <div className="w-20">{e.max_ta}</div>
-                    <div className="w-20">{e.day}</div>
-                    <div className="w-20">{e.time_from}</div>
-                    <div className="w-20">{e.time_to}</div>
-                </div>
-            ))}
-        </div>
-    );
 };
 
-const DueDate = ({ className }) => {
+const NewAddTable = forwardRef(({ index, ...props }, register) => {
+    return (
+        <div className="">
+            <input type="text" className="border-2 border-text" {...register(`tables[${index}].section`)} />
+            <input type="text" className="border-2 border-text" {...register(`tables[${index}].time_to`)} />
+            <input type="text" className="border-2 border-text" {...register(`tables[${index}].time_from`)} />
+            <input type="text" className="border-2 border-text" {...register(`tables[${index}].max_ta`)} />
+        </div>
+    );
+});
+
+const DueDate = ({ register }) => {
     const [startDate, setStartDate] = useState(new Date());
     return (
-        <div className={`flex w-[220px] flex-col items-center space-y-6 ${className}`}>
+        <div className={`flex w-[220px] flex-col items-center space-y-6 `}>
             <div className="font-bold text-secondary underline ">กำหนดวันปิดรับสมัคร</div>
             <div className="relative flex w-full items-center">
                 <input type="date" className="w-full rounded-md border-2 px-2 py-[0.375rem]" />
